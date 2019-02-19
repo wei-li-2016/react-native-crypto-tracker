eda9dc7983142dca87d5d6f32805e10b31630b1a
PineGolden311 <software.developer112@gmail.com>
2019-02-19 07:27:59 +0400

    step1

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Header } from './src/components';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>This is my react native tutorial</Text>
+      <View>
+        <Header />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
