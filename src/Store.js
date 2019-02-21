2e4c7108b52679466e9abf6eb3dbb0ee640c7d23
PineGolden311 <software.developer112@gmail.com>
2019-02-21 16:45:13 +0400

    Create Store.js

---
+++ src/Store.js
@@ -0,0 +1,22 @@
+import { Platform } from 'react-native'
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from 'redux';
+import devTools from 'remote-redux-devtools'
+import promise from 'redux-promise'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+
+import RootReducer from './Reducers'
+
+const middleware = applyMiddleware(thunk, promise, logger);
+
+const Store = createStore(
+  RootReducer,
+  compose(
+    middleware,
+    devTool
+  )
+)
